94bce0c49ae01349cc0b9807abd3df3b117c23f1
gexinpai <783263503@qq.com>
2024-07-29 09:58:39 +0800

    feat: iconfont前缀http

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 const EditorIcon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_4396576_5e1efp8q63.js', // 在 iconfont.cn 上生成
+  scriptUrl: 'https://at.alicdn.com/t/c/font_4396576_5e1efp8q63.js', // 在 iconfont.cn 上生成
 });
 
 export default EditorIcon;

a0ca42ad1b63a66af38ec4f6ebf302ee61ab1b69
gexinpai <783263503@qq.com>
2024-11-07 22:20:44 +0800

    test: 恢复撤销前

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 const EditorIcon = createFromIconfontCN({
-  scriptUrl: 'https://at.alicdn.com/t/c/font_4396576_7hyg3okrvrt.js', // 在 iconfont.cn 上生成, 需要加http，不然打包后不能访问；
+  scriptUrl: 'https://at.alicdn.com/t/c/font_4396576_67c5shv6xat.js', // 在 iconfont.cn 上生成, 需要加http，不然打包后不能访问；
 });
 
 export default EditorIcon;
